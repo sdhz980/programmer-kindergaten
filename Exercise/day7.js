7d19ba855e49f1ae5a682ab86e398ee0641a2679
SDHZed <83025970+sdhz980@users.noreply.github.com>
2024-02-16 20:05:13 +0000

    day7

---
+++ Exercise/day7.js
@@ -0,0 +1,57 @@
+// Exercise
+
+// ● Create a function to check if two objects are equal
+// ● Example
+//     ○ Input : { a: 2 } & { a: 1 }
+//     ○ Output: false
+// ● Example
+//     ○ Input : { a: “Hello” } & { a: 1 }
+//     ○ Output: false
+// ● Example
+//     ○ Input : { a: 1 } & { a: 1 }
+//     ○ Output: true
+
+
+
+// Exercise
+
+// ● Create a function to get the intersection of two objects
+// ● Example
+//     ○ Input : { a: 1, b: 2 } & { a: 1, c: 3 }
+//     ○ Output: { a: 1 }
+
+// Exercise
+
+// ● Create a function to merge two array of student data and remove duplicate data
+// ● Student data : name & email
+// ● Example :
+//         Array1 → [
+//         { name: ‘Student 1’, email : ‘student1@mail.com’ },
+//         { name: ‘Student 2’, email : ‘student2@mail.com’ }
+//         ]
+//         Array2 → [
+//         { name: ‘Student 1’, email : ‘student1@mail.com’ },
+//         { name: ‘Student 3’, email : ‘student3@mail.com’ }
+//         ]
+// ● Result :
+
+//     ArrayResult → [
+//         { name: ‘Student 1’, email : ‘student1@mail.com’ },
+//         { name: ‘Student 2’, email : ‘student2@mail.com’ },
+//         { name: ‘Student 3’, email : ‘student3@mail.com’ }
+//         ]
+
+// Exercise
+
+// ● Create a function that can accept input as an array of objects and switch all values into property and
+// property into value
+// ● Example :
+//     ○ Input : [{ name: ‘David’, age: 20 }]
+//     ○ Output : [{ David: ‘name’, 20: ‘age’}]
+
+// Exercise
+
+// ● Create a function to find a factorial number using recursion
+// ● Example
+//     ○ Input : 5
+//     ○ Output: 5! = 5 x 4 x 3 x 2 x 1 = 120
