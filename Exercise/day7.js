b73a8e4534e1c682ffcc820e7b29917bd8ccc57d
SDHZed <sadamhack@gmail.com>
2024-02-18 16:44:09 +0700

    day7

--- Exercise/day7.js
+++ Exercise/day7.js
@@ -11,6 +11,23 @@
 //     ○ Input : { a: 1 } & { a: 1 }
 //     ○ Output: true
 
+const obj1 = { a : 2 , b: 1};
+const obj2 = { a : 2 };
+
+function checkEqual (ob1,ob2) {
+    let isEqual = false;
+
+    if (Object.keys(ob1) && Object.keys(ob2)) {
+        for (const [key,value] of Object.entries(ob1)) {
+            if (ob1[key] == ob2[key]) {
+                isEqual = true
+            }   else return isEqual = false;
+        }
+    }
+    return isEqual    
+}
+
+console.log(checkEqual(obj1,obj2))
 
 
 // Exercise
@@ -19,6 +36,24 @@
 // ● Example
 //     ○ Input : { a: 1, b: 2 } & { a: 1, c: 3 }
 //     ○ Output: { a: 1 }
+
+const objEct1 = {a: 1, b: 2};
+const objEct2 = {a: 1, c: 3};
+
+function findIntersection (obj1,obj2) {
+    const tmp1 = Object.keys(obj1);
+    const tmp2 = Object.keys(obj2);
+    let result = '';
+
+    console.log(tmp1);
+    console.log(tmp2);
+    for (let i = 0;i < tmp1.length;i++) {
+        
+    }
+
+}
+
+console.log(findIntersection(objEct1,objEct2))
 
 // Exercise
 
@@ -48,6 +83,27 @@
 // ● Example :
 //     ○ Input : [{ name: ‘David’, age: 20 }]
 //     ○ Output : [{ David: ‘name’, 20: ‘age’}]
+
+const exObj = [{ name : 'David', age: 20}]
+const resRevObj = [{}]
+
+function reverseObject(obj) {
+    for (let i = 0; i< obj.length;i++) {
+        let tmpKeys = Object.keys(obj[i]);
+        let tmpArray = [];
+        
+        for (let a = 0; a< tmpKeys.length; a ++) {
+            tmpArray.push(tmpKeys[a],obj[i][tmpKeys[a]])
+            console.log(tmpArray);
+            resRevObj[i][tmpArray[1]] = tmpArray[0];
+            tmpArray = [];
+        }
+        
+    }
+}
+
+reverseObject(exObj);
+console.log(resRevObj);
 
 // Exercise
 
